4d076fdbb7661d7f2a89a0dafa4e5907381e603e
MichaelStyle <omnislash@hotmail.com>
2015-07-16 15:48:31 -0400

    - Smoke is spawning at the bottom of the screen, going up at a fixed speed and is following the camera

--- Assets/Scripts/Smoke.cs
+++ Assets/Scripts/Smoke.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Smoke : MonoBehaviour {
+public class Smoke : MonoBehaviour 
+{
+	// Variables for Smoke
+	public float mSpeed;
+	public float mDistanceMade;
+	public bool mIsRaising;
 
-	// Use this for initialization
-	void Start () {
-	
+	// Camera
+	public Camera mCam;
+	public float mCamHeight;
+	public Vector3 mCamPos;
+
+	void Start () 
+	{
+		Init ();	
+	}
+
+	void Init()
+	{
+		// Values
+		mSpeed = 0.05f;
+		mDistanceMade = 0.0f;
+		mIsRaising = true;
+
+		// Cam
+		mCam = Camera.main;
+		mCamHeight = mCam.orthographicSize;
+
+		// Get the camera's inital position
+		UpdateCamPos();
+
+		// Place Smoke as the bottom of the camera)
+		transform.position = new Vector3(mCamPos.x, mCamPos.y - (mCamHeight * 2), 0.0f);
+	}
+
+	void Update () 
+	{
+		UpdateCamPos();
+		Raise ();
+	}
+
+	private void UpdateCamPos()
+	{
+		mCamPos = mCam.transform.position;  
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	private void Raise()
+	{
+		transform.position = new Vector3(mCamPos.x, mCamPos.y - (mCamHeight * 1.6f) + mDistanceMade, 0.0f);
+
+		if (mIsRaising)
+		{
+			mDistanceMade += mSpeed * Time.deltaTime;
+		}
 	}
 }
