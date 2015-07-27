09c74860813703ffdba5ca660c70156de5f6556a
Spardason <eric-vaillancourt@hotmail.ca>
2015-07-27 16:48:42 -0400

    Gameflow modification fo rthe ending screens

--- Assets/Scripts/GameFlow.cs
+++ Assets/Scripts/GameFlow.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -12,8 +13,29 @@ public class GameFlow : MonoBehaviour {
 	private const string LEVEL_2 = "LEVEL_TWO";
 	private const string LEVEL_3 = "LEVEL_THREE";
 	private const string LOADING = "SCREEN_LOADING";
-	private const string VICTORY = "SCREEN_VICTORY";
-	private const string DEFEAT = "SCREEN_DEFEAT";
+	private const string END = "SCREEN_END";
+
+    // Images
+    public Image WIN_1; 
+    public Image WIN_2; 
+    public Image WIN_3; 
+    public Image LOSE_1;
+    public Image LOSE_2;
+    public Image LOSE_3;
+    public Image PARTIAL_1; 
+    public Image PARTIAL_2; 
+    public Image PARTIAL_3; 
+
+    // Local Images variables
+    private Image winImage1;
+    private Image winImage2;
+    private Image winImage3;
+    private Image loseImage1;
+    private Image loseImage2;
+    private Image loseImage3;
+    private Image insImage1;
+    private Image insImage2;
+    private Image isnImage3;
 
 	private System.Random mRnd;
 	private int mlevelIndex;
@@ -122,10 +144,12 @@ public class GameFlow : MonoBehaviour {
 				Application.LoadLevel(LEVEL_3);
 				break;
 			case (States.VICTORY):
-				Application.LoadLevel(VICTORY);
+                SetEndBG();
+				Application.LoadLevel(END);
 				break;
 			case (States.DEFEAT):
-				Application.LoadLevel(DEFEAT);
+                SetEndBG();
+				Application.LoadLevel(END);
 				break;
 			default:
 				break;
@@ -162,4 +186,11 @@ public class GameFlow : MonoBehaviour {
 		ChangeLevel(States.MENU);
 		UnPause();
 	}
+
+    private void SetEndBG()
+    {
+        GameObject temp = GameObject.Find("BackGround");
+        Image tempImage = temp.GetComponent<Image>();
+        tempImage = WIN_1;
+    }
 }
